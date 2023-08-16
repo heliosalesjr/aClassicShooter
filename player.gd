70976dd66f694978946c17514393dd4fe664a6a4
heliosalesjr <heliosalesjr@gmail.com>
2023-08-16 17:48:04 -0300

    bullet + shooting

--- player.gd
+++ player.gd
@@ -1,7 +1,21 @@
 extends Area2D
 
-@onready var screensize = get_viewport_rect().size
+
 @export var speed = 150
+
+@export var cooldown = 0.25
+@export var bullet_scene : PackedScene
+
+var can_shoot = true
+
+@onready var screensize = get_viewport_rect().size
+
+func _ready():
+	start()
+
+func start():
+	position = Vector2(screensize.x / 2, screensize.y - 64)
+	$GunCooldown.wait_time = cooldown
 
 func _process(delta):
 	var input = Input.get_vector("left", "right", "up", "down")
@@ -16,3 +30,20 @@ func _process(delta):
 		$Ship/Boosters.animation = "forward"
 	position += input * speed * delta
 	position = position.clamp(Vector2(8, 8), screensize-Vector2(8, 8))
+	if Input.is_action_pressed("shoot"):
+		shoot()
+		
+		
+func shoot():
+	if not can_shoot:
+		return
+	can_shoot = false
+	$GunCooldown.start()
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start(position + Vector2(0, -8))
+
+
+
+func _on_gun_cooldown_timeout():
+	can_shoot = true
